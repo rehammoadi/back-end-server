65df60ea532b255b63b0e4521681f1f72339930b
Reham <rehammoady@hotmail.com>
2019-05-25 20:39:09 +0300

    test on api requests

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,4 +30,20 @@ Route::post('/signup',function (){
     return response()->json(array(
         'id'=>1
     ));
+});
+
+Route::post('/login',function (Request $request){
+
+   // $req = json_decode($request->getContent(), true);
+    $req =  $request->all();
+    if($req['username'] && $req['password'] ){
+        return response()->json(
+               ['userData'=>['work'=>'work']]//$request->all()
+        );
+    }else{
+        return response()->json(
+              ['userData'=>[]]//$request->all()
+            
+        );
+    }
 });
